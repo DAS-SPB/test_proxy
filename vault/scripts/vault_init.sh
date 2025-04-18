0b029152dbc22518ef8b858ea6b3bd633e2820b3
AntonioMont <garsiamarkes111@gmail.com>
2025-04-18 14:16:00 +0300

    vault init script

---
+++ vault/scripts/vault_init.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+set -e
+
+VAULT_ADDR="http://vault:8200"
+VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID}"
+
+# 1) waiting fot vault to start
+until curl --silent --fail $VAULT_ADDR/v1/sys/health >/dev/null 2>&1; do
+  sleep 1
+done
+
+export VAULT_ADDR VAULT_TOKEN
+
+# 2) app1 secrets
+vault kv put secret/app1 \
+    token_secret_key="${APP1_TOKEN_SECRET_KEY}" \
+    token_algorithm="${APP1_TOKEN_ALG}" \
+    token_issuer="${APP1_TOKEN_ISSUER}" \
+    token_audience="${APP1_TOKEN_AUD}" \
+    token_expiration_time="${APP1_TOKEN_EXPIRATION}" \
+    api_key="${APP1_API_KEY}" \
+    app2_api_key="${APP1_APP2_API_KEY}" \
+    app2_url="${APP1_APP2_URL}"
+
+# 3) proxy secrets
+vault kv put secret/proxy \
+    app2_url="${PROXY_APP2_URL}"
+
+# 4) app2 secrets
+vault kv put secret/app2 \
+    api_key="${APP2_API_KEY}"
